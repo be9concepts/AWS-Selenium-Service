229c07eb836fc9420a62a26ceaab74fb3e65af08
Joshua Caputo <jos2244923@yahoo.com>
2017-07-16 11:15:43 -0700

    Add files via upload

--- Selenium-IFTTT/ifttt-watcher.sh
+++ Selenium-IFTTT/ifttt-watcher.sh
@@ -10,20 +10,19 @@ export DISPLAY=:99
 # Load __init__.py with python2.7
 python2.7 __init__.py
 
-URL="http://be9concepts.com/Selenium-IFTTT/notify.php"
 RUN='true'
 echo waiting...
 while [[ $RUN = 'true' ]]; do
-  RESPONSE=$(curl -s  $URL)
-  #echo $RESPONSE
+  RESPONSE=$(curl -H "Content-Type: application/json" -X POST -d "{\"operation\":\"GET\",\"ID\":\"101\""}  https://l7ufle33og.execute-api.us-east-2.amazonaws.com/Testing/submitdata)
+  echo $RESPONSE
   NEWRESPONSE=$(cut -d "." -f1 <<< $RESPONSE)
   NEWRESPONSE2=$(cut -d "." -f2 <<< $RESPONSE)
-
-  if [[ "$NEWRESPONSE" == 'true' ]]; then
+  echo $NEWRESPONSE
+  if [[ "$NEWRESPONSE" == '1' ]]; then
        echo running command $NEWRESPONSE2
        sudo python2.7 tests/$NEWRESPONSE2.py
        echo resetting server state
-       curl -s 'https://be9concepts.com/Selenium-IFTTT/update.php?state=false&command=none'
+       curl -H "Content-Type:ion/json" -X POST -d "{\"operation\":\"PUT\",\"id\":\"101\",\"state\":\"-1\",\"command\":\"none\""}  https://l7ufle33og.execute-api.us-east-2.amazonaws.com/Testing/submitdata
        echo done
        echo waiting again...
        sleep 10
---
+++ Selenium-IFTTT/tests/arm-alarm.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class ArmAlarm(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.yalehomesystem.co.uk/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_arm_alarm(self):
+        driver = self.driver
+        driver.get(self.base_url + "/homeportal/html/")
+        driver.find_element_by_id("id").clear()
+        driver.find_element_by_id("id").send_keys("matthew.constable@gmail.com")
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("P4s5w0rdyle")
+        driver.find_element_by_css_selector("button.ui-btn-hidden").click()
+        driver.find_element_by_id("mode_home_1").click()
+        driver.find_element_by_id("mode_disarm_1").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
