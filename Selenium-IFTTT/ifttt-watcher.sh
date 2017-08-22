9e8964d6a7041761e7e120b388f9d6279b6a025c
Joshua Caputo <jos2244923@yahoo.com>
2017-08-22 10:27:39 -0700

    Update ifttt-watcher.sh

--- Selenium-IFTTT/ifttt-watcher.sh
+++ Selenium-IFTTT/ifttt-watcher.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 # Selenium Bash Script
 # Created by @Be9Concepts on Monday, July 10th, 2017
+# Last updated on August 4th, 2017 by @Be9Concepts
 
-# Start Services Required
-
-Xvfb :99 -screen 0 1024x768x24 -ac 2<&1 </dev/null &
-export DISPLAY=:99
-
-# Load __init__.py with python2.7
-python2.7 __init__.py
+sudo python2.7 /home/ubuntu/Selenium-IFTTT/__init__.py
 
 RUN='true'
-echo waiting...
 while [[ $RUN = 'true' ]]; do
-  RESPONSE=$(curl -H "Content-Type: application/json" -X POST -d "{\"operation\":\"GET\",\"ID\":\"101\""}  https://l7ufle33og.execute-api.us-east-2.amazonaws.com/Testing/submitdata)
-  echo $RESPONSE
+  RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"operation\":\"GET\",\"ID\":\"101\""}  https://YOURAPI.execute-api.us-east-2.amazonaws.com/Testing/submitdata)
+  # echo $RESPONSE
   NEWRESPONSE=$(cut -d "." -f1 <<< $RESPONSE)
   NEWRESPONSE2=$(cut -d "." -f2 <<< $RESPONSE)
-  echo $NEWRESPONSE
+  # echo $NEWRESPONSE
   if [[ "$NEWRESPONSE" == '1' ]]; then
-       echo running command $NEWRESPONSE2
-       sudo python2.7 tests/$NEWRESPONSE2.py
-       echo resetting server state
-       curl -H "Content-Type:ion/json" -X POST -d "{\"operation\":\"PUT\",\"id\":\"101\",\"state\":\"-1\",\"command\":\"none\""}  https://l7ufle33og.execute-api.us-east-2.amazonaws.com/Testing/submitdata
-       echo done
-       echo waiting again...
-       sleep 10
+      Xvfb :99 -screen 0 1024x768x24 -ac 2<&1 </dev/null &
+      export DISPLAY=:99
+      echo running the command $NEWRESPONSE2.py
+      python2.7 /home/ubuntu/Selenium-IFTTT/tests/$NEWRESPONSE2.py >> /home/output_$NEWRESPONSE2.log
+      echo resetting server state in DynamoDB
+      curl -H "Content-Type:ion/json" -X POST -d "{\"operation\":\"PUT\",\"id\":\"101\",\"state\":\"-1\",\"command\":\"none\""}  https://l7ufle33og.execute-api.us-east-2.amazonaws.com/Testing/submitdata
+      echo done
+      # echo waiting again...
+      sleep 10
   else
     # response is false
       #echo waiting 5 seconds...
